940787888f7e454aa97f95b6fc84697c42f09cbf
Vegard Sandengen <vegardsandengen@gmail.com>
2017-01-24 22:11:24 +0100

    videoplay: now reports overlay messages and loads stream entries from server

--- videoplay/proto-videostream.ts
+++ videoplay/proto-videostream.ts
@@ -6,8 +6,6 @@ class VideoStreamElement {
     // The <video/> container element to stream media source into.
     private elementVideo: any;
 
-    private elementOverlay: any;
-
     // The MediaSource used to feed the video.
     public mediaSource: any;
 
@@ -25,21 +23,17 @@ class VideoStreamElement {
 
         // Clear everything previously declared within
         self.elementParent.innerHTML = "";
+        self.elementParent.classList.add('proto-videostream-content');
+        self.setOverlayText("Initializing");
 
-        // Create and append overlay element
-        self.elementOverlay = document.createElement('div');
-        self.elementParent.appendChild(self.elementOverlay);
-
-        // Create and append video element
-        let tmpDiv = document.createElement('div');
-        self.elementParent.appendChild(tmpDiv);
+        // Create and append element
         self.elementVideo = document.createElement('video');
         self.elementVideo.setAttribute('autoplay', '');
-        tmpDiv.appendChild(self.elementVideo);
+        self.elementParent.appendChild(self.elementVideo);
 
         window.MediaSource = window.MediaSource || window.WebKitMediaSource;
         if (!!!window.MediaSource) {
-            // TODO: Populate overlay message
+            self.setOverlayText("MediaSource API unavailable");
             throw new Error('MediaSource API is not available');
         }
 
@@ -51,15 +45,55 @@ class VideoStreamElement {
 
         // Clean-up the error
         self.elementVideo.addEventListener('error', self.errorHandler);
+
+        self.setOverlayText("");
+    }
+
+    public setOverlayText(text: string) {
+        let self = this;
+
+        self.elementParent.setAttribute('data-overlay-text', text);
+
+        // We control the padding through data-overlay-padding attribute
+        // We do this to create the overlay with a "nice" formated message,
+        // whilst removing it when we actually have video to show (ugly little box if not)
+        if (text == "") {
+            self.elementParent.setAttribute('data-overlay-padding', "none");
+        } else {
+            self.elementParent.setAttribute('data-overlay-padding', "block");
+        }
     }
 
     private errorHandler(e: Event) {
         let self = this;
 
+        self.setOverlayText("Unexpected error");
         console.log("Media element raised erroenous event");
         console.log(e);
         self.elementVideo.removeEventListener('error', self.errorHandler);
         // TODO: Destroy all registerd streams
+
+        throw new Error("Unexpected Media error");
+    }
+}
+
+class StreamEntry {
+    // Name of this stream entry
+    public name: string;
+
+    // Comma separated list of codecs, or something
+    // XXX CLARIFY
+    public codecs: string;
+
+    // The type of video this stream entry is: webm
+    public format: string;
+
+    public constructor(name: string, format: string, codecs: string) {
+        let self = this;
+
+        self.name = name;
+        self.format = format;
+        self.codecs = codecs;
     }
 }
 
@@ -77,14 +111,19 @@ class VideoStreamSource {
     // The source type string. e.g, 'video/webm; codecs="vorbis, vp8"'
     private sourceType: string;
 
-    // The VideoStreamElement who handles this video source
-    private vse: VideoStreamElement;
+    private format: string;
+    private codecs: string;
 
-    public constructor(vse: VideoStreamElement) {
+    // The VideoStreamElement who handles this video source
+    private video: VideoStreamElement;
+
+    public constructor(video: VideoStreamElement, format: string, codecs: string) {
         let self = this;
 
-        self.vse = vse;
-        self.sourceType = 'video/webm; codecs="vp8"';
+        self.video = video;
+        self.format = format;
+        self.codecs = codecs;
+        self.sourceType = `video/${format}; codecs="${codecs}"`;
 
         // Bind event handlers so 'this' references class instead of event
         self.openHandler = self.openHandler.bind(self);
@@ -93,10 +132,10 @@ class VideoStreamSource {
         self.abortHandler = self.abortHandler.bind(self);
 
         // Create the source buffer once media  source is open
-        if (self.vse.mediaSource.readyState === 'open') {
+        if (self.video.mediaSource.readyState === 'open') {
             self.createSourceBuffer();
         } else {
-            self.vse.mediaSource.addEventListener('sourceopen', self.openHandler);
+            self.video.mediaSource.addEventListener('sourceopen', self.openHandler);
         }
     }
 
@@ -120,10 +159,12 @@ class VideoStreamSource {
         let self = this;
 
         if (!window.MediaSource.isTypeSupported(self.sourceType)) {
+            let text = `Unsupported format ${self.format} (codecs: ${self.codecs}`
+            self.video.setOverlayText(text);
             throw Error('Unsupported type ' + self.sourceType);
         }
 
-        self.sourceBuffer = self.vse.mediaSource.addSourceBuffer(self.sourceType);
+        self.sourceBuffer = self.video.mediaSource.addSourceBuffer(self.sourceType);
         self.sourceBuffer.mode = 'sequence';
         self.sourceBuffer.addEventListener('updateend', self.updateendHandler);
 
@@ -137,7 +178,6 @@ class VideoStreamSource {
 
         if (self.sourceBuffer == null) {
             throw new Error("SourceBuffer undefined");
-            return;
         }
         if (self.bufferQueue.length == 0) {
             return;
@@ -154,7 +194,7 @@ class VideoStreamSource {
     private openHandler(e: Event) {
         let self = this;
 
-        self.vse.mediaSource.removeEventListener('sourceopen', self.openHandler);
+        self.video.mediaSource.removeEventListener('sourceopen', self.openHandler);
         self.createSourceBuffer();
     }
 
@@ -185,13 +225,13 @@ class ProtoVideoStream {
     public selectedStreamEntry: string;
 
     //
-    private streamEntries: Map<string, boolean>;
+    private streamEntries: Map<string, StreamEntry>;
 
     // The active websocket connection for this instance
     private websocket: any;
 
     // The internal stream element allocated
-    private vse: VideoStreamElement;
+    private video: VideoStreamElement;
 
     // Internal allocation of a source buffer to feed the video stream element
     public videoStreamSource: VideoStreamSource;
@@ -199,7 +239,7 @@ class ProtoVideoStream {
     public constructor(id: string) {
         let self = this;
 
-        self.vse = new VideoStreamElement(id);
+        self.video = new VideoStreamElement(id);
     }
 
     private send (message) {
@@ -243,10 +283,17 @@ class ProtoVideoStream {
 
                     case messageType.STREAM_ENTRIES:
                         console.log("proto: stream entries");
-                        // TODO: populate array with array from protobuf msg
-                        self.streamEntries = new Map<string, boolean>();
-                        //TEMPORARY
-                        self.streamEntries["parrot"] = true
+
+                        self.streamEntries = new Map<string, StreamEntry>();
+
+                        let entries = msg.getStreamEntries().getEntriesList();
+                        for (let i = 0; i < entries.length; i++) {
+                            let entry = entries[i];
+                            let struct = new StreamEntry(entry.getName(),
+                                                         entry.getFormat(),
+                                                         entry.getCodec());
+                            self.streamEntries.set(entry.getName(), struct);
+                        }
 
                         self.changeSelectedVideoEntry();
                         break;
@@ -263,6 +310,7 @@ class ProtoVideoStream {
         // Handle 'onclose'
         this.websocket.onclose = function () {
             console.log("websocket onclose");
+            self.video.setOverlayText("Connection closed...");
         }
     }
 
@@ -284,12 +332,10 @@ class ProtoVideoStream {
         let self = this;
 
         console.log("Changing selected stream entry: " + self.selectedStreamEntry);
-        // TODO: Panic if entryId is not in internal list retrieved from server.
 
         if (self.streamEntries.has(self.selectedStreamEntry) == false) {
-            // TODO: Add text to video overlay
-            //console.log("Stream entry " + self.selectedStreamEntry + " unavailable");
-            //return;
+            self.video.setOverlayText("Stream '" + self.selectedStreamEntry +"' unavailable");
+            throw new Error("Stream entry '"+ self.selectedStreamEntry + "' unavailable");
         }
 
         // Remove active stream source buffer
@@ -297,10 +343,10 @@ class ProtoVideoStream {
             // TODO: Remove active sourceBuffer (if active)
         }
 
-        // TODO: Get codec type from streamEntries and feed this to videostreamsource
-        // XXX: streamEntries should probably be a map
-        self.videoStreamSource = new VideoStreamSource(self.vse);
+        let entry = self.streamEntries.get(self.selectedStreamEntry);
+        self.videoStreamSource = new VideoStreamSource(self.video, entry.format, entry.codecs);
 
+        // Send our selection to the server!
         var message = new proto.videostream.FromClient;
         message.setSelectStreamEntry(self.selectedStreamEntry);
         self.send(message);
